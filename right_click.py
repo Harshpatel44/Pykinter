658d55a7750f2c3a9d4130f05a0dba2e977dc767
Harshgp44 <harshgp44@gmail.com>
2018-03-15 20:44:01 +0530

    doc file added

--- right_click.py
+++ right_click.py
@@ -6,4 +6,5 @@ def menu(event,root):
     def first_time():
         canvas=tk.Canvas(root,height=150,width=120,background="#dddddd",bd=0)
         canvas.place(x=widget.winfo_x()+event.x,y=widget.winfo_y()+event.y)
+
     first_time()
