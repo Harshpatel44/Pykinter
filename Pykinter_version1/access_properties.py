02f46824155f9f6230ca974f2c83700ac1e8b13e
Harsh Patel <hr402607@dal.ca>
2020-09-14 13:29:30 -0300

    repository empty

--- Pykinter_version1/access_properties.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'Harsh'
-import properties_tab
-
-def set_properties(widget):
-    print(widget.cget("text"))
-
-    properties_tab.ne.set(widget.cget("text"))
-    properties_tab.name_enter.update()
-    print("done for this thing")
